ca03a7eb591d0a97219decbb5ee0e5837ae2c8d6
Arthur Delobel <arthur.delobel.etu@univ-lille.fr>
2023-09-04 16:38:49 +0200

    Piece

--- src/main/Piece.java
+++ src/main/Piece.java
@@ -1,12 +1,16 @@
 public enum Piece{
+    PIECE_A(new boolean[][]{{true}}),
+    PIECE_B(new boolean[][]{{true,true}}),
+    PIECE_C(new boolean[][]{{true,true,true}}),
+    PIECE_D(new boolean[][]{{true,true},{false,true}});
 
-    UN(),DEUX,TROIS,QUATRE,CINQ;
-
-    String name;
-    boolean[][] form;
+    private boolean[][] form;
     
-    Piece(String name, boolean[][] form){
-        this.name=name;
+    Piece(boolean[][] form){
         this.form=form;
+    }
+
+    public boolean[][] getForm(){
+        return this.form;
     }
 }
